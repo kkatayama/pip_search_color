0d31d3111655c63a18ca8991a72fff0dac5507e6
kkatayama <katayama@udel.edu>
2021-05-13 05:13:57 -0400

    set default=2 for pages

--- pip_search/__init__.py
+++ pip_search/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 # -- pip-search-color version
-__version__ = "0.2.5"
+__version__ = "0.2.6"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='pip-search-color',
-    version='0.2.5',
+    version='0.2.6',
     author='Teddy Katayama',
     author_email='katayama@udel.edu',
     description='Wrapper for "pip search" with color and hyperlink features.',
